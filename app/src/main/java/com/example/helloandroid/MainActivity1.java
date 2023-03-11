dbe624ae792815c3e386f436b30f35ec55f74e13
Eric Poblete <ericpoblete123@gmail.com>
2023-03-11 13:08:58 +0800

    123

--- app/src/main/java/com/example/helloandroid/MainActivity1.java
+++ app/src/main/java/com/example/helloandroid/MainActivity1.java
@@ -1,6 +1,9 @@
 package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
 import android.os.Bundle;
@@ -12,5 +15,12 @@ public class MainActivity1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
         Button button = (Button) findViewById(R.id.btn2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity1.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/helloandroid/MainActivity3.java
+++ app/src/main/java/com/example/helloandroid/MainActivity3.java
@@ -2,6 +2,9 @@ package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity3 extends AppCompatActivity {
@@ -10,5 +13,13 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Button button = (Button) findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
+                startActivity(intent);
+            }
+        });
     }
 }
