2c59d751cc0cee96717d59143bb0d1dda0acd049
Eric Poblete <ericpoblete123@gmail.com>
2023-03-11 13:02:59 +0800

    123

--- app/src/main/java/com/example/helloandroid/MainActivity1.java
+++ app/src/main/java/com/example/helloandroid/MainActivity1.java
@@ -1,7 +1,8 @@
 package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
+import android.os.Bundle;
 import android.os.Bundle;
 
 public class MainActivity1 extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class MainActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
+        Button button = (Button) findViewById(R.id.btn2);
     }
 }
