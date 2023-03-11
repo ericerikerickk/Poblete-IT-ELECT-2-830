6c20e93fd761c74f01a86c5e492b0998fcda2082
Eric Poblete <ericpoblete123@gmail.com>
2023-03-11 11:37:13 +0800

    110323

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button firstFragment, secondFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // get the reference of button's
+        firstFragment = (Button) findViewById(R.id.button);
+        secondFragment = (Button) findViewById(R.id.button2);
     }
 }
